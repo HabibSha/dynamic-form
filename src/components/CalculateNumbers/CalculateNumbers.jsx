bd4955ff70557ffc7851692560fedf294a0fb633
habibsha <habibshahalahi7424@gmail.com>
2024-08-17 23:15:52 +0600

    Create feature functionality

--- src/components/CalculateNumbers/CalculateNumbers.jsx
+++ src/components/CalculateNumbers/CalculateNumbers.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 
+const generateId = () => {
+  return Math.round(Math.random() * 19969) + 1;
+};
+
 const CalculateNumbers = () => {
   const [inputState, setInputState] = useState({ a: 0, b: 0 });
   const [result, setResult] = useState(0);
+  const [history, setHistory] = useState([]);
 
   const handleChange = (e) => {
     setInputState({
@@ -16,6 +21,7 @@ const CalculateNumbers = () => {
       a: 0,
       b: 0,
     });
+    setResult(0);
   };
 
   const mathematicsFunc = (operators) => {
@@ -23,8 +29,22 @@ const CalculateNumbers = () => {
       "operators",
       `return ${inputState.a} ${operators} ${inputState.b}`
     );
-    setResult(f(operators));
+
+    if (inputState.a === 0 && inputState.b === 0) {
+      console.log("Input without value");
+    } else {
+      setResult(f(operators));
+    }
     // setResult(eval(`${inputState.a} + ${operators} + ${inputState.b}`));
+
+    const sum = `${inputState.a} ${operators} ${inputState.b}`;
+    const historyBody = {
+      id: generateId(),
+      sum,
+      result: f(operators),
+    };
+
+    setHistory([...history, historyBody]);
   };
 
   return (
@@ -52,8 +72,22 @@ const CalculateNumbers = () => {
           <button onClick={() => mathematicsFunc("-")}>-</button>
           <button onClick={() => mathematicsFunc("*")}>*</button>
           <button onClick={() => mathematicsFunc("/")}>/</button>
+          <button onClick={() => mathematicsFunc("%")}>%</button>
           <button onClick={clearFunc}>clear</button>
         </div>
+        <br />
+        {history.length > 0 ? (
+          <ul>
+            {history.map((item) => (
+              <li key={item.id}>
+                <p>Sum: {item.sum}</p>
+                <p>Result: {item.result}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <small>There is no history</small>
+        )}
       </div>
     </>
   );
